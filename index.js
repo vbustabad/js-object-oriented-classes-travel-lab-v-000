b328c9820f7ad4ff201a5ccfb896b01bcad13476
Veronica Bustabad <vbustabad@gmail.com>
2018-09-10 16:30:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+class Driver {
+  constructor(name, startDate) {
+    this.name = name;
+    this.startDate = new Date(startDate);
+  }
 
+  yearsExperienceFromBeginningOf(year) {
+    return (year - this.startDate.getFullYear() - 1);
+  }
+}
+
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+
+//   blocksTravelled() {
+//     return ((`${this.beginningLocation.horizontal}` - `${this.endingLocation.horizontal}`) + (`${this.beginningLocation.vertical}` - `${this.endingLocation.vertical}`));
+//   }
+//
+//   estimatedTime() {
+//     return ;
+//   }
+// }
